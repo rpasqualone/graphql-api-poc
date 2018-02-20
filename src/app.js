de04f12f33ab1b41d89a48a77401e28a34d857d9
Richard Pasqualone <richard@gingerbreadshed.com>
2018-02-20 15:57:23 -0700

    cherry pic work for routes (cherry picked from commit 52a8155)

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
-import index from './routes/index';
-import users from './routes/users';
+import routes from './routes';
 
 const app = express();
 
@@ -19,8 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(...routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
